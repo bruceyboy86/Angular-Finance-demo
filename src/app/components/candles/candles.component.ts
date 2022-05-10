b5135e1ef93aaa6f02a609cf24f8a1c24c7d6b7b
bruceyboy86 <bruced247@gmail.com>
2022-05-10 23:43:43 +0100

    looking to combine candle results with a utility function

--- src/app/components/candles/candles.component.ts
+++ src/app/components/candles/candles.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 import { ICandles } from 'src/app/modals/modals';
+import * as Highcharts from 'highcharts';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-candles',
@@ -9,11 +11,42 @@ import { ICandles } from 'src/app/modals/modals';
 })
 export class CandlesComponent implements OnInit {
   constructor(private http: HttpService) {}
-  candles!: ICandles;
+  candles!: any;
+
+  public options: any = {
+    chart: {},
+    title: {
+      text: 'Sample Scatter Plot',
+    },
+    credits: {
+      enabled: false,
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {},
+    },
+    series: [
+      {
+        name: 'Normal',
+        // data: [[new Date('2018-01-25 18:38:31').getTime(), 2]],
+        // data: [this.candles.c],
+      },
+      {
+        name: 'Abnormal',
+        data: [[new Date('2018-02-05 18:38:31').getTime(), 7]],
+      },
+    ],
+  };
 
   ngOnInit(): void {
     this.http.getCandles('AMZN').subscribe((res) => {
+      // this.candles = zip(res.c, res.t, [res.l]);
+      // res.map((r: any) =>
+      // );
+      // https://underscorejs.org/#zip
+
       this.candles = res;
     });
+    Highcharts.chart('container', this.options);
   }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -10,8 +10,6 @@ import { Observable } from 'rxjs';
 export class HttpService {
   constructor(private http: HttpClient) {}
 
-  
-
   getProfile(company: string): Observable<IProfile> {
     return this.http.get<IProfile>(
       `${env.BASE_URL}stock/profile2/?symbol=` + company + `&token=${env.TOKEN}`
@@ -24,8 +22,8 @@ export class HttpService {
     );
   }
 
-  getCandles(company: string): Observable<ICandles> {
-    return this.http.get<ICandles>(
+  getCandles(company: string): Observable<any> {
+    return this.http.get<any>(
       `${env.BASE_URL}stock/candle?symbol=` +
         company +
         `&resolution=60&from=1651187922&to=1651787922&token=${env.TOKEN}`
